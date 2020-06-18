969ba77e96babfcc355a71b54b2457ad9fb7ced5
Aditya Bhangle <aditya.bhangle@trianz.com>
2020-06-18 09:28:32 +0000

    first commit

---
+++ pkg/apis/adi658/group.go
@@ -0,0 +1,6 @@
+// Package adi658 contains adi658 API versions.
+//
+// This file ensures Go source parsers acknowledge the adi658 package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package adi658
---
+++ pkg/apis/adi658/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the adi658 v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=adi658.com
+package v1alpha1
